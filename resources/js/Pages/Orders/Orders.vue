fcf96dae4ecde8db26c98a813090282d83ceb370
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2023-04-14 15:38:49 +0200

    Add controllers + begin Inertia

---
+++ resources/js/Pages/Orders/Orders.vue
@@ -0,0 +1,35 @@
+
+<script>
+import { defineComponent } from 'vue'
+import { usePage } from '@inertiajs/inertia-vue3'
+
+export default defineComponent({
+    setup() {
+        const { props } = usePage().component
+        const orders = props.orders
+
+        return { orders }
+    }
+})
+</script>
+<template>
+    <div>
+        <h1>Orders</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Customer Name</th>
+                    <th>Customer Email</th>
+                    <th>Total Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="order in orders" :key="order.id">
+                    <td>{{ order.customer_name }}</td>
+                    <td>{{ order.customer_email }}</td>
+                    <td>{{ order.total_amount }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,36 @@
+<script>
+import { defineComponent } from 'vue'
+import { usePage } from '@inertiajs/inertia-vue3'
+
+export default defineComponent({
+    setup() {
+        const { props } = usePage().component
+        const products = props.products
+
+        return { products }
+    }
+})
+</script>
+<template>
+    <div>
+        <h1>Products</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.id">
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.description }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.stock }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
